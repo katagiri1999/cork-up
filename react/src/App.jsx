4c77f9676368fc5e39ec43eba9b4352b38f18b91
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-11-22 20:26:49 +0900

    add input form

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import "./styles/index.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const login = () => {
+  const [value_id, set_id_value] = useState('');
+  const [value_pw, set_pw_value] = useState('');
+
+  const handle_id_change = (event) => {
+    set_id_value(event.target.value);
+  };
+  const handle_pw_change = (event) => {
+    set_pw_value(event.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <title>Login</title>
+      <h1 id="Pagetitle">Login</h1>
 
-export default App
+      <label>
+        ID:
+        <input
+          type="text"
+          value={value_id}
+          onChange={handle_id_change}
+        />
+        <br/>
+        PW:
+        <input
+          type="text"
+          value={value_pw}
+          onChange={handle_pw_change}
+        />
+      </label>
+      <p>ID: {value_id}</p>
+      <p>PW: {value_pw}</p>
+    </div>
+  );
+};
+
+export default login;
