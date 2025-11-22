48a69cc76f000eb6a4fedc6ca3bb407aeff7eb57
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-11-22 22:15:27 +0900

    update login page

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,41 +1,89 @@
-import React, { useState } from 'react';
-import "./styles/index.css";
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+  Alert,
+} from "@mui/material";
 
-const login = () => {
-  const [value_id, set_id_value] = useState('');
-  const [value_pw, set_pw_value] = useState('');
-
-  const handle_id_change = (event) => {
-    set_id_value(event.target.value);
+function App() {
+  const [valueEmail, setEmailValue] = useState("");
+  const handleEmailChange = (event) => {
+    setEmailValue(event.target.value);
   };
-  const handle_pw_change = (event) => {
-    set_pw_value(event.target.value);
+
+  const [valuePw, setPwValue] = useState("");
+  const handlePwChange = (event) => {
+    setPwValue(event.target.value);
+  };
+
+  const onClickSignin = () => {
+    console.log(valueEmail);
+    console.log(valuePw);
+
+    // 疑似的にログインの成否をランダムで決定
+    var random = parseInt(Math.random() * 100);
+    var _error = random % 2 === 0;
+    console.log(_error);
+    setError(_error);
+  };
+
+  const [error, setError] = useState(false);
+  useEffect(() => { }, [error]);
+  function IsError() {
+    if (error) {
+      return (
+        <Alert severity="error">Invalid Email or PW</Alert>
+      );
+    };
   };
 
   return (
-    <div>
-      <title>Login</title>
-      <h1 id="Pagetitle">Login</h1>
+    <Container maxWidth="xs">
+      <title>Sign in</title>
+      <Box
+        sx={{ marginTop: 5 }}
+      >
+        <Typography component="h1" variant="h4">
+          Sign in
+        </Typography>
 
-      <label>
-        ID:
-        <input
-          type="text"
-          value={value_id}
-          onChange={handle_id_change}
-        />
-        <br/>
-        PW:
-        <input
-          type="text"
-          value={value_pw}
-          onChange={handle_pw_change}
-        />
-      </label>
-      <p>ID: {value_id}</p>
-      <p>PW: {value_pw}</p>
-    </div>
+        <Box component="form" noValidate sx={{ marginTop: "1%" }}>
+          <TextField
+            value={valueEmail}
+            onChange={handleEmailChange}
+            margin="normal"
+            fullWidth
+            label="email"
+          />
+
+          <TextField
+            value={valuePw}
+            onChange={handlePwChange}
+            margin="normal"
+            fullWidth
+            label="password"
+            type="password"
+            autoComplete="current-password"
+          />
+
+          <Button
+            onClick={onClickSignin}
+            fullWidth
+            variant="contained"
+            sx={{ marginTop: "5%", marginBottom: "2%" }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+
+      <IsError />
+
+    </Container>
   );
 };
 
-export default login;
+export default App;
