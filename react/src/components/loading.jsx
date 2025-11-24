50d9e3d737e8b48e05b57dec5e1ec6edcda53049
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-11-24 22:32:30 +0900

    update indent settings

--- react/src/components/loading.jsx
+++ react/src/components/loading.jsx
@@ -5,27 +5,27 @@ import screenStore from '../store/screen_store.jsx';
 
 
 function Loading() {
-    const { isLoading } = screenStore();
+  const { isLoading } = screenStore();
 
-    useEffect(() => {
-        const loading = document.getElementById('LOADING');
+  useEffect(() => {
+    const loading = document.getElementById('LOADING');
 
-        if (isLoading) {
-            loading.style.display = 'initial';
-            loading.style.opacity = '1';
-        } else {
-            loading.style.display = 'none';
-            document.body.style.overflow = 'auto';
-        }
-    }, [isLoading]);
+    if (isLoading) {
+      loading.style.display = 'initial';
+      loading.style.opacity = '1';
+    } else {
+      loading.style.display = 'none';
+      document.body.style.overflow = 'auto';
+    }
+  }, [isLoading]);
 
-    return (
-        <div id="LOADING">
-            <div id="LOADING-ANIMATION">
-                <div id="LOADING-ANIMATION-CIRCLE"></div>
-            </div>
-        </div>
-    );
+  return (
+    <div id="LOADING">
+      <div id="LOADING-ANIMATION">
+        <div id="LOADING-ANIMATION-CIRCLE"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
