f929f4052ae0aac97fecc67b453b14fb40d5f768
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-11-24 19:35:15 +0900

    add header func

--- react/src/components/header.jsx
+++ react/src/components/header.jsx
@@ -1,31 +1,85 @@
 import MenuIcon from '@mui/icons-material/Menu';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-function Header() {
+
+function DrawerAppBar() {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const navItems = ['Information', 'Contact'];
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h5" sx={{ my: 2 }}>
+        Menu
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
+    <Box>
+      <AppBar position="static" sx={{ mb: 7 }}>
+        <Toolbar style={{ display: "flex" }}>
           <IconButton
-            size="large"
-            edge="start"
             color="inherit"
-            aria-label="menu"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            size='large'
             sx={{ mr: 2 }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography
+            variant="h6"
+          >
             Cork-Up
           </Typography>
         </Toolbar>
       </AppBar>
+        <Drawer
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          sx={{
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 300 },
+          }}
+        >
+          {drawer}
+        </Drawer>
     </Box>
   );
 }
 
-export default Header;
+DrawerAppBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default DrawerAppBar;
