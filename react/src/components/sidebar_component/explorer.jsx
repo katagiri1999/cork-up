f9164313e7bd89cbaec0bd0a457fcd190e20d51b
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-12-06 19:19:07 +0900

    update

--- react/src/components/sidebar_component/explorer.jsx
+++ react/src/components/sidebar_component/explorer.jsx
@@ -4,7 +4,11 @@ import {
 } from "@mui/material";
 import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
 
+import userStore from "../../store/user_store";
+
 function Explorer() {
+  const { id_token } = userStore();
+
   const tree = [
     {
       id: 'main',
@@ -31,17 +35,23 @@ function Explorer() {
     },
   ];
 
-  return (
-    <>
-      <Typography variant="h5" sx={{ my: 2 }}>
-        Explorer
-      </Typography>
+  if (id_token) {
+    return (
+      <>
+        <Typography variant="h5" sx={{ my: 2 }}>
+          Explorer
+        </Typography>
 
-      <Box sx={{ m: 5 }}>
-        <RichTreeView items={tree} />
-      </Box>
-    </>
-  );
+        <Box sx={{ m: 5 }}>
+          <RichTreeView items={tree} />
+        </Box>
+      </>
+    );
+  } else {
+    return (
+      <></>
+    );
+  }
 };
 
 export default Explorer;
