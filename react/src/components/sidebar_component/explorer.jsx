b4da1c1c9f8a8e2342341b66377059ebb746d2e9
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-12-06 23:22:47 +0900

    update

--- react/src/components/sidebar_component/explorer.jsx
+++ react/src/components/sidebar_component/explorer.jsx
@@ -6,34 +6,11 @@ import {
   Typography,
 } from "@mui/material";
 import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
-import { useEffect } from 'react';
 
-import screen_store from "../../store/screen_store";
 import userStore from "../../store/user_store";
-import * as utils from "../../utils.js";
-import Loading from '../loading.jsx';
 
 function Explorer() {
-  const { id_token, tree, setTree } = userStore();
-  const { isLoading, setLoading } = screen_store();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      var res = await utils.requests(
-        `${utils.API_HOST}/${utils.API_VER}/trees`,
-        "GET",
-        { Authorization: `Bearer ${id_token}` },
-        {}
-      );
-      setTree(res.tree);
-      setLoading(false);
-    };
-
-    if (id_token && !tree) {
-      fetchData();
-    };
-  }, []);
+  const { id_token, tree } = userStore();
 
   const handleItemClick = (_, itemId) => {
     if (itemId.endsWith('.md')) {
@@ -43,11 +20,9 @@ function Explorer() {
     }
   };
 
-  if (id_token) {
+  if (id_token && tree) {
     return (
       <>
-        <Loading loading={isLoading} />
-
         <Typography variant="h5" sx={{ my: 2 }}>
           Explorer
         </Typography>
--- react/src/views/main.jsx
+++ react/src/views/main.jsx
@@ -2,15 +2,42 @@ import {
   Box,
   Container,
 } from "@mui/material";
+import { useEffect } from 'react';
 
 import Header from "../components/header.jsx";
+import Loading from "../components/loading.jsx";
+import screenStore from "../store/screen_store.jsx";
+import userStore from "../store/user_store.jsx";
+import * as utils from "../utils.js";
 
 function Main() {
+  const { id_token, setTree } = userStore();
+  const { isLoading, setLoading } = screenStore();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      var res = await utils.requests(
+        `${utils.API_HOST}/${utils.API_VER}/trees`,
+        "GET",
+        { Authorization: `Bearer ${id_token}` },
+        {}
+      );
+      setTree(res.tree);
+      setLoading(false);
+    };
+
+    if (id_token) {
+      fetchData();
+    };
+  }, []);
 
   return (
     <>
       <title>Main</title>
       <Header />
+      <Loading loading={isLoading} />
+
       <Container maxWidth="xs">
         <Box
           sx={{ marginTop: 5 }}
