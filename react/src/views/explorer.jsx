dd996f4729da07beba176423c33857e38a1364fd
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-12-06 17:21:32 +0900

    add tree

--- react/src/views/explorer.jsx
+++ react/src/views/explorer.jsx
@@ -3,10 +3,37 @@ import {
   Container,
   Typography,
 } from "@mui/material";
+import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
+
 
 import Header from "../components/header.jsx";
 
-function Menu() {
+function Explorer() {
+  const tree = [
+    {
+      id: 'main',
+      label: 'main',
+      children: [
+        { id: 'main-community', label: 'hogehoge1' },
+        { id: 'main-pro', label: 'hogehoge2' },
+        { id: 'main-premium', label: 'hogehoge3' },
+      ],
+    },
+    {
+      id: 'sub',
+      label: 'sub',
+      children: [
+        { id: 'sub-community', label: 'hogehoge4' },
+        { id: 'sub-pro', label: 'hogehoge5' },
+        {
+          id: 'sub-premium', label: 'hogehoge6', children:
+            [
+              { id: 'sub-premium-pro', label: 'hogehoge7' }
+            ]
+        },
+      ],
+    },
+  ];
 
   return (
     <>
@@ -20,10 +47,14 @@ function Menu() {
             Explorer
           </Typography>
 
+          <Box sx={{ minHeight: 352, minWidth: 250 }}>
+            <RichTreeView items={tree} />
+          </Box>
+
         </Box>
       </Container>
     </>
   );
 };
 
-export default Menu;
+export default Explorer;
