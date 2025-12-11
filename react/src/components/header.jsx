9eb7473bfd63d31f64f1f2c3d8a7f48601c04893
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-12-11 20:20:39 +0900

    dev

--- react/src/components/header.jsx
+++ react/src/components/header.jsx
@@ -2,16 +2,21 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
+import userStore from '../store/user_store.jsx';
+
 import Profile from './header_component/profile.jsx';
 import Sidebar from './header_component/sidebar.jsx';
 
 function Header() {
+  const { id_token } = userStore();
 
   return (
     <AppBar position="static" sx={{ mb: 7 }}>
       <Toolbar style={{ display: "flex" }}>
 
-        <Sidebar />
+        {id_token &&
+          <Sidebar />
+        }
 
         <Typography
           variant="h6"
@@ -24,6 +29,7 @@ function Header() {
       </Toolbar>
     </AppBar>
   );
+
 }
 
 export default Header;
